14a03d4262a5baef53165ec3b1dd93f727ed45c3
T13489145349 <59318748+T13489145349@users.noreply.github.com>
2021-08-25 20:45:08 +0800

    Update Test.java

--- src/main/java/org/example/git/Test.java
+++ src/main/java/org/example/git/Test.java
@@ -9,5 +9,6 @@ public class Test {
         System.out.println("hello4 master");
         System.out.println("hello4 hot fix2 ");
         System.out.println("push test ");
+        System.out.println("pull test ");
     }
 }
