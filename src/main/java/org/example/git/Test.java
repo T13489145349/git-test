6d8efdf485e775fbc276b342798dfd0e16dd3bbb
T13489145349 <59318748+T13489145349@users.noreply.github.com>
2021-08-25 20:58:40 +0800

    Update Test.java

--- src/main/java/org/example/git/Test.java
+++ src/main/java/org/example/git/Test.java
@@ -11,5 +11,6 @@ public class Test {
         System.out.println("push test ");
         System.out.println("pull test ");
         System.out.println("push clone test ");
+        System.out.println(" pull test ");
     }
 }
